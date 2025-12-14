3927904c4a9e1cc1715157be70ce4c8b907beecf
diyasunil911-star <diyasunil911@gmail.com>
2025-12-14 08:08:01 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,10 +6,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+import fetch from "node-fetch";
+
 app.post("/send-sos", async (req, res) => {
-  console.log("SOS endpoint hit");
-  res.json({ success: true });
+  try {
+    const { location } = req.body;
+
+    const response = await fetch("https://www.fast2sms.com/dev/bulkV2", {
+      method: "POST",
+      headers: {
+        "authorization": process.env.FAST2SMS_API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        route: "q",
+        message: `🚨 SOS ALERT!\nI need help.\nLocation: ${location}`,
+        language: "english",
+        flash: 0,
+        numbers: "8078416898"
+      })
+    });
+
+    const data = await response.json();
+    console.log("SMS Response:", data);
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "SMS failed" });
+  }
 });
+
 
 
 // 🔴 PUT YOUR FAST2SMS API KEY HERE
